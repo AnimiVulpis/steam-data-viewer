c71e4dbfca448931fc8dbec1f498964fcef39e06
AnimiVulpis <animi.vulpis@gmail.com>
2015-01-01 23:42:38 +0100

    add d3.js for viz; first display on view page

--- extension/background.js
+++ extension/background.js
@@ -16,6 +16,8 @@ var displayRules = {
 };
 
 function collectOwnGames() {
+//	check if I can get the source of the calling of this function
+//	as an alternative to using chrome.extension.getViews...?
 	function communityPageLoaded(response) {
 		var userPageUrl = response.target.response.querySelector(".menuitem.supernav.username").getAttribute("href").slice(0, -5);
 		var xhr = new XMLHttpRequest();
@@ -25,7 +27,7 @@ function collectOwnGames() {
 		xhr.send();
 	}
 	function gamePageLoaded(response) {
-//		splitting the lines first seems (further tests needed) to give better performance
+//		splitting the lines first seems (further tests needed) to yield better performance
 		var gameListLine = response.target.response.querySelector("script[language]").textContent.split("\n")[1];
 		var startIndex = gameListLine.indexOf("[");
 		var endIndex = gameListLine.indexOf("];");
@@ -34,6 +36,10 @@ function collectOwnGames() {
 			previousVal[list[index].appid] = currentVal;
 			return previousVal;
 		}, {});
+//		add error handling; in case the tab does not exist anymore, if any error handling is necessary
+		var extensionViewPage = chrome.extension.getViews({type: "tab"})[0];
+		extensionViewPage.viewdata.gameList = gameList;
+		extensionViewPage.viewdata.showGames();
 	}
 	var xhr = new XMLHttpRequest();
 	xhr.onload = communityPageLoaded;
--- extension/viewdata/viewdata.js
+++ extension/viewdata/viewdata.js
@@ -1,3 +1,37 @@
 "use strict";
-var gatherButton = document.getElementById("gatherData");
-gatherButton.addEventListener("click", chrome.extension.getBackgroundPage().collectOwnGames);
+
+var viewdata = function viewdata() {
+	var exports = {};
+	var gameList;
+	exports.gameList = gameList;
+	var gatherButton = document.getElementById("gatherData");
+	gatherButton.addEventListener("click", chrome.extension.getBackgroundPage().collectOwnGames);
+	function showGames() {
+		var thing = {};
+		var dataviz = d3.select("#dataviz");
+		dataviz.selectAll("div")
+			.data(this.gameList)
+			.enter()
+			.append("div")
+			.classed("gameItem", true)
+			.append("p")
+			.classed("gameTitle", true)
+			.text(function addData(datum) {
+				for (var item in datum) {
+					thing[item] = (typeof thing[item] === "undefined" ? 1 : thing[item] + 1);
+				}
+				return datum.name;
+			});
+		console.log(thing);
+	}
+	exports.showGames = showGames;
+	return exports;
+}();
+
+//gameInfo['link'] = ( "http://steamcommunity.com/app/" + gameInfo['appid'] );
+//href="#{profile_link}/stats/#{friendlyURL}/?tab=achievements"
+//href="#{profile_link}/stats/#{friendlyURL}/?tab=stats"
+//href="#{profile_link}/stats/#{friendlyURL}/?tab=leaderboards"
+//href="http://steamcommunity.com/stats/#{friendlyURL}/achievements/"
+//href="http://steamcommunity.com/stats/#{friendlyURL}/leaderboards/"
+//d3.select("#dataviz").selectAll("div").data(gameList).enter().append("div").append("p").text(function(a,b,c,d,e,f){debugger; return a.name});
